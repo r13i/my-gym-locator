585848dc2e18ae5aff1fcf7be4fe19e71cec2963
Redouane Achouri <achouri.a.r@gmail.com>
2019-06-22 14:20:11 +0100

    Dockerized Flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
